bae3d96763761f4f08eaccfe2c2b611b8f0c441d
Maryam Javed <maryamjavedshk@gmail.com>
2024-09-12 08:05:30 -0700

    withdraw fn accessed by owner through modifer

--- FundMe.sol
+++ FundMe.sol
@@ -7,17 +7,25 @@ contract FundMe{
     using PriceConverter for uint256;
      uint256 mininmumUSD=50 * 1e18;
      mapping(address=>uint256) public addressToAmountFunded;
+     address public owner;
+
+    constructor(){
+        owner=msg.sender;    //is going to be whoever is deploying the contract
+    }
+
 
 //where people who send fund addresses will be stored
      address[] public funders;
     function fund() public payable{
-    //msg.value is considered frst parameter in any library fn so we don't have to pass value in fn argument
+    //msg.value is considered first parameter in any library fn so we don't have to pass value in fn argument
     require(msg.value.getConversionRate()>=mininmumUSD, "didn't send enough"); 
        funders.push(msg.sender);
     addressToAmountFunded[msg.sender]+=msg.value;
     }
 
-    function withdraw() public{
+    function withdraw() public onlyOwner{
+//withdraw fn only accessed by owner
+        require(msg.sender==owner, "Sender is not owner");
         for(uint256 fIndex=0; fIndex<funders.length; fIndex++){
             address funderAddress=funders[fIndex];
             addressToAmountFunded[funderAddress]=0;
@@ -26,7 +34,15 @@ contract FundMe{
     //reset array
     // start w 0th element in an array
     funders=new address[](0);
+
+    (bool callSuccess, )=payable(msg.sender).call{value:address(this).balance}("");
+    require(callSuccess, "call failed");
     }
+
+    modifier onlyOwner{
+      require(msg.sender==owner, "Sender is not owner");
+      _;  
+    } 
 }
 
 
